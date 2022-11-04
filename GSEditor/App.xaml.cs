3abff5cfc553ffd0235838a67e3d802c71e57b2d
koipkoi <hrh6914@gmail.com>
2022-11-04 10:48:08 +0900

    창 설정 추가

--- GSEditor/App.xaml.cs
+++ GSEditor/App.xaml.cs
@@ -7,19 +7,34 @@ namespace GSEditor;
 
 public partial class App
 {
-  protected override void OnStartup(StartupEventArgs e)
-  {
-    base.OnStartup(e);
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
 
-    Injector.Register(this);
-    Injector.Register(new Pokegold());
-    Injector.Register(new AppSettings());
+        _ = Injector.Register(this);
+        _ = Injector.Register(new Pokegold());
+        var appSettings = Injector.Register(new AppSettings());
 
-    AeroTheme.SetAsCurrentTheme();
+        AeroTheme.SetAsCurrentTheme();
 
-    var window = new MainWindow();
-    window.ShowDialog();
+        var window = new MainWindow
+        {
+            WindowState = appSettings.WindowState,
+            Left = appSettings.WindowLeft,
+            Top = appSettings.WindowTop,
+            Width = appSettings.WindowWidth,
+            Height = appSettings.WindowHeight,
+            MinWidth = AppSettings.WindowMinWidth,
+            MinHeight = AppSettings.WindowMinHeight,
+        };
+        window.ShowDialog();
 
-    Shutdown();
-  }
+        appSettings.WindowLeft = window.Left;
+        appSettings.WindowTop = window.Top;
+        appSettings.WindowWidth = window.Width;
+        appSettings.WindowHeight = window.Height;
+        appSettings.WindowState = window.WindowState;
+
+        Shutdown();
+    }
 }
